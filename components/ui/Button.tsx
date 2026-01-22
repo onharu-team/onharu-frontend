39e318379bbaca6d3faa378e314aabf22f286cd3
yeaseula <kminea@naver.com>
2026-01-22 21:00:46 +0900

    fix: tailwind extend 수정

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,5 +1,11 @@
-export const Button = () => {
-    return (
-        <></>
-    )
+interface ButtonProps {
+  varient: "light" | "default" | "dark";
+  fontSize: "sm" | "md" | "lg";
+  width: "xs" | "sm" | "md" | "lg";
+  height: "xs" | "sm" | "md" | "lg";
+  children: React.ReactNode;
 }
+
+export const Button = ({ varient = "default", fontSize, width, height, children }: ButtonProps) => {
+  return <></>;
+};

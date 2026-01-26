97198339a41e9902eaf780ec6d4c5422f3568de7
yeaseula <kminea@naver.com>
2026-01-26 19:06:10 +0900

    ♿️ 슬라이드 탭 접근성 개선 : 안내문구 추가

--- app/charitystore/[id]/page.tsx
+++ app/charitystore/[id]/page.tsx
@@ -3,14 +3,16 @@ import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { Like } from "@/components/feature/StoreLike";
 import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+import { RiKeyboardFill } from "@remixicon/react";
 
 export default function Detail() {
   const viewportRef = useRef<HTMLDivElement>(null);
   const trackRef = useRef<HTMLDivElement>(null);
   const testRef = useRef<HTMLDivElement>(null);
   const maxdragRef = useRef(0);
-  const [slidearea, setSlidearea] = useState({ left: 0, right: 0 });
   const progressRef = useRef(0);
+  const [slidearea, setSlidearea] = useState({ left: 0, right: 0 });
+  const [tabInfor, setTabInfor] = useState(false);
   const x = useMotionValue(0);
   const smoothX = useSpring(x, { stiffness: 300, damping: 30 });
 
@@ -61,76 +63,94 @@ export default function Detail() {
     }
   };
 
+  const handleTabInforOpen = () => {
+    if (!tabInfor) setTabInfor(true);
+  };
+
+  const handleTabInforClose = () => {
+    if (tabInfor) setTabInfor(false);
+  };
+
   return (
     <section className="mt-section-sm-top md:mt-section-lg-top mb-section-sm-bottom md:mb-section-lg-bottom">
       <div className="wrapper">
-        <div className="mb-7 flex gap-2 border-b border-gray-300 pb-7">
+        <div className="flex gap-2 border-b border-gray-300 pb-4">
           <p className="text-2xl font-bold">스토어 네임</p>
           <Like isLiked />
         </div>
-        <motion.div ref={viewportRef} className="w-full overflow-hidden">
-          <motion.div
-            ref={testRef}
-            drag="x"
-            tabIndex={0}
-            style={{ x: smoothX }}
-            onKeyDown={handleKeyDown}
-            role="region"
-            dragMomentum={false}
-            dragConstraints={slidearea}
-            dragElastic={0.1}
-          >
-            <div className="flex gap-5" ref={trackRef}>
-              <motion.div className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
-                <Image
-                  src={"/image/page/test-image.png"}
-                  alt=""
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="pointer-events-none"
-                />
-              </motion.div>
-              <motion.div tabIndex={0} className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
-                <Image
-                  src={"/image/page/test-image.png"}
-                  alt=""
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="pointer-events-none"
-                />
-              </motion.div>
-              <motion.div tabIndex={0} className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
-                <Image
-                  src={"/image/page/test-image.png"}
-                  alt=""
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="pointer-events-none"
-                />
-              </motion.div>
-              <motion.div tabIndex={0} className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
-                <Image
-                  src={"/image/page/test-image.png"}
-                  alt=""
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="pointer-events-none"
-                />
-              </motion.div>
-              <motion.div tabIndex={0} className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
-                <Image
-                  src={"/image/page/test-image.png"}
-                  alt=""
-                  fill
-                  style={{ objectFit: "cover" }}
-                  className="pointer-events-none"
-                />
-              </motion.div>
-            </div>
+        <div className="relative pt-8">
+          {tabInfor && (
+            <p className="text-main absolute top-1 left-0 z-10 flex items-center gap-1.5 text-sm font-bold">
+              <RiKeyboardFill size={17} />
+              키보드를 좌우로 움직여 매장 사진을 구경하세요!
+            </p>
+          )}
+          <motion.div ref={viewportRef} className="w-full overflow-hidden">
+            <motion.div
+              ref={testRef}
+              drag="x"
+              tabIndex={0}
+              style={{ x: smoothX }}
+              onKeyDown={handleKeyDown}
+              role="region"
+              dragMomentum={false}
+              dragConstraints={slidearea}
+              dragElastic={0.1}
+              onFocus={() => handleTabInforOpen()}
+              onBlur={() => handleTabInforClose()}
+            >
+              <div className="flex gap-5" ref={trackRef}>
+                <motion.div className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
+                  <Image
+                    src={"/image/page/test-image.png"}
+                    alt=""
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className="pointer-events-none"
+                  />
+                </motion.div>
+                <motion.div className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
+                  <Image
+                    src={"/image/page/test-image.png"}
+                    alt=""
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className="pointer-events-none"
+                  />
+                </motion.div>
+                <motion.div className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
+                  <Image
+                    src={"/image/page/test-image.png"}
+                    alt=""
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className="pointer-events-none"
+                  />
+                </motion.div>
+                <motion.div className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
+                  <Image
+                    src={"/image/page/test-image.png"}
+                    alt=""
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className="pointer-events-none"
+                  />
+                </motion.div>
+                <motion.div className="relative h-[340px] w-[calc(25%-15px)] shrink-0">
+                  <Image
+                    src={"/image/page/test-image.png"}
+                    alt=""
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className="pointer-events-none"
+                  />
+                </motion.div>
+              </div>
+            </motion.div>
           </motion.div>
-        </motion.div>
-        <div className="mt-4 h-1 w-full overflow-hidden rounded-full bg-gray-200">
-          <motion.div className="bg-main-400 h-full" style={{ width: progressWidth }} />
+          <div className="mt-4 h-1 w-full overflow-hidden rounded-full bg-gray-200">
+            <motion.div className="bg-main-400 h-full" style={{ width: progressWidth }} />
+          </div>
         </div>
       </div>
     </section>

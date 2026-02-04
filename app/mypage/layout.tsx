144c300b72c3586c289c7fa1a82c5a420cf7684d
lsohyuniil <lim07so10h@gmail.com>
2026-02-05 01:56:24 +0900

    style: maypage layout

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -4,7 +4,7 @@ export default async function MyPageLayout({ children }: { children: React.React
   const user = { role: "owner" as const };
 
   return (
-    <section className="mt-section-sm-top md:mt-section-lg-top mb-section-sm-bottom md:mb-section-lg-bottom">
+    <section className="lg:mt-section-lg-top mb-section-sm-bottom lg:mb-section-lg-bottom">
       <div className="wrapper flex min-h-screen flex-col lg:flex-row">
         <Sidebar role={user.role} />
         <main className="flex-1 py-4 sm:p-6">{children}</main>

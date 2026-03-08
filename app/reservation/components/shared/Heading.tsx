1bf9b37c58ba84f9f616190ab2215508b70974ff
lsohyuniil <lim07so10h@gmail.com>
2026-03-09 02:39:22 +0900

    feat: PNG 이미지 SVG로 교체

--- app/reservation/components/shared/Heading.tsx
+++ app/reservation/components/shared/Heading.tsx
@@ -4,7 +4,7 @@ export const Heading = ({ title }: { title: string }) => {
   return (
     <div className="flex items-center gap-2 md:gap-4">
       <div className="relative h-[25px] w-[30px] md:h-[33px] md:w-[38px]">
-        <Image src="/image/page/reservation-img2.png" alt="" fill style={{ objectFit: "cover" }} />
+        <Image src="/image/page/reservation-img2.svg" alt="" fill style={{ objectFit: "cover" }} />
       </div>
       <p className="md:text-md text-sm font-semibold">{title}</p>
     </div>

8ce4812f4f0d1397a2f55ce11425bfc3c9ed9953
lsohyuniil <lim07so10h@gmail.com>
2026-03-15 00:53:24 +0900

    feat: 아동/가게 카카오 소셜 로그인 이미지 업로드 및 회원가입 API 연동

--- app/oauth-signup/child/page.tsx
+++ app/oauth-signup/child/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { SignupSuccessModal } from "@/app/signup/components/SignupSuccessModal";
 import { SignupFormValues } from "@/app/signup/types";
 import DocumentUploadField from "@/components/feature/DocumentUploadField";
+import { Toast } from "@/components/feature/toast/Toast";
 import { FIELD_CONFIG } from "@/components/form-fields/fieldConfig";
 import { FormField } from "@/components/form-fields/FormField";
 import FormLayout from "@/components/layout/FormLayout";
 import { Button } from "@/components/ui/Button";
+import { useUploadImage } from "@/hooks/useUploadImage";
+import { signupKakaoChild } from "@/lib/api/auth";
+import { ImageInfo } from "@/lib/api/types/auth";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
@@ -16,24 +22,76 @@ export default function OauthChildSignupPage() {
     formState: { errors },
     watch,
     setError,
+    clearErrors,
   } = useForm<SignupFormValues>({ mode: "onSubmit" });
 
   const [documents, setDocuments] = useState<File[]>([]);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
-  const onSubmit = (data: SignupFormValues) => {
+  const { mutateAsync: uploadImage } = useUploadImage();
+  const router = useRouter();
+
+  // 이미지 업로드 + 회원가입 처리
+  const handleSignup = async (data: SignupFormValues) => {
     if (documents.length === 0) {
       setError("document", { type: "manual", message: "증명서류를 업로드해주세요." });
       return;
     }
 
-    // console.log("회원가입 데이터:", finalData);
+    clearErrors("document");
+
+    try {
+      // 이미지 업로드
+      const uploadedUrl = await uploadImage(documents[0]);
+      const images: ImageInfo[] = [
+        {
+          fileKey: uploadedUrl.split("/onharu-minio/")[1],
+          filePath: uploadedUrl,
+          displayOrder: "0",
+        },
+      ];
+
+      // 회원가입 API 호출
+      const response = await signupKakaoChild({
+        nickname: data.nickname!,
+        images,
+      });
+
+      if (response.success) {
+        setIsOpenModal(true);
+      } else {
+        Toast(
+          "error",
+          "회원가입에 실패했습니다.",
+          response.message || "잠시 후 다시 시도해주세요."
+        );
+
+        console.error("회원가입 실패:", response.message);
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+      if (err.status === 400) {
+        setError("nickname", {
+          type: "manual",
+          message: err.message || "사용할 수 없는 닉네임입니다.",
+        });
+      } else {
+        console.error("회원가입/업로드 에러:", err);
+        Toast("error", "회원가입에 실패했습니다.", err?.message || "잠시 후 다시 시도해주세요.");
+      }
+    }
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+    router.push("/login");
   };
 
   return (
     <div className="wrapper flex min-h-screen items-center justify-center">
       <div className="w-full max-w-115">
         <FormLayout title="아동 회원가입">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+          <form onSubmit={handleSubmit(handleSignup)} className="flex flex-col gap-5">
             {/* 닉네임 */}
             <FormField<SignupFormValues>
               name="nickname"
@@ -56,12 +114,16 @@ export default function OauthChildSignupPage() {
               />
             </div>
 
+            {/* 회원가입 버튼 */}
             <div className="mt-2.5 sm:mt-7.5">
               <Button type="submit" varient="default" width="lg" height="md" fontSize="md">
                 회원가입
               </Button>
             </div>
           </form>
+
+          {/* 성공 모달 */}
+          <SignupSuccessModal isOpen={isOpenModal} onClose={handleCloseModal} />
         </FormLayout>
       </div>
     </div>
--- app/oauth-signup/owner/page.tsx
+++ app/oauth-signup/owner/page.tsx
@@ -9,9 +9,15 @@ import { Button } from "@/components/ui/Button";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { signupKakaoOwner } from "@/lib/api/auth";
+import { SignupSuccessModal } from "@/app/signup/components/SignupSuccessModal";
+import { useRouter } from "next/navigation";
+import { Toast } from "@/components/feature/toast/Toast";
 
 export default function OauthOwnerSignupPage() {
   const [isBusinessVerified, setIsBusinessVerified] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const router = useRouter();
 
   const {
     register,
@@ -22,7 +28,8 @@ export default function OauthOwnerSignupPage() {
     getValues,
   } = useForm<SignupFormValues>({ mode: "onSubmit" });
 
-  const onSubmit = async (data: SignupFormValues) => {
+  // 회원가입 처리
+  const handleSignup = async (data: SignupFormValues) => {
     if (!isBusinessVerified) {
       setError("businessNumber", {
         type: "manual",
@@ -38,20 +45,31 @@ export default function OauthOwnerSignupPage() {
       });
 
       if (response.success) {
-        console.log("카카오 가게 회원가입 성공:", response.data);
+        setIsOpenModal(true);
       } else {
         console.error("회원가입 실패:", response.message);
+        Toast(
+          "error",
+          "회원가입에 실패했습니다.",
+          response.message || "잠시 후 다시 시도해주세요."
+        );
       }
     } catch (err) {
-      console.error("회원가입 API 호출 중 오류:", err);
+      console.error("회원가입 에러:", err);
+      Toast("error", "회원가입에 실패했습니다.", "잠시 후 다시 시도해주세요.");
     }
+  };
+
+  const handleCloseModal = () => {
+    setIsOpenModal(false);
+    router.push("/login");
   };
 
   return (
     <div className="wrapper flex min-h-screen items-center justify-center">
       <div className="w-full max-w-115">
         <FormLayout title="매장 회원가입">
-          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-5">
+          <form onSubmit={handleSubmit(handleSignup)} className="flex flex-col gap-5">
             {/* 매장명 */}
             <FormField<SignupFormValues>
               name="storeName"
@@ -71,12 +89,16 @@ export default function OauthOwnerSignupPage() {
               setIsBusinessVerified={setIsBusinessVerified}
             />
 
+            {/* 회원가입 버튼 */}
             <div className="mt-2.5 sm:mt-7.5">
               <Button type="submit" varient="default" width="lg" height="md" fontSize="md">
                 회원가입
               </Button>
             </div>
           </form>
+
+          {/* 성공 모달 */}
+          <SignupSuccessModal isOpen={isOpenModal} onClose={handleCloseModal} />
         </FormLayout>
       </div>
     </div>
